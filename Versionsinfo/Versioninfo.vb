2ef4289f35d0cced123124cc1de35b4c37016448
K. Schr <KSchr@users.noreply.github.com>
2016-06-01 08:32:42 +0200

    Upload Versioninfo.vb

---
+++ Versionsinfo/Versioninfo.vb
@@ -0,0 +1,10 @@
+﻿<Microsoft.VisualBasic.ComClass()> Public Class Versionsinfo
+
+    <System.Runtime.InteropServices.ComVisible(True)> _
+    Public Function getVersioninfo(ByVal Mode As Int32, ByVal Anwendung As String, ByVal Version As String, ByVal VersionBis As String) As String()
+
+        Return IgetVersionsinfo(Mode, Anwendung, Version, VersionBis)
+
+    End Function
+
+End Class
